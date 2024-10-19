9365ef68835f3bd9a446215a6674352dd5e7a768
mmandelstrom <mmandelstrom@gmail.com>
2024-10-19 14:11:57 +0200

    Added texttype enum and textnode class

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import Textype, TextNode
+from enum import Enum
+
+def main():
+
+    node = TextNode("This is my first node", "bold", "https://boot.dev")
+    print(node)
+    
+main()
